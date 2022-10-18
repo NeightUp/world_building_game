c1a59bf86164d0ba69ec34261df7b64df100f378
Neight <picturen8@yahoo.com>
2022-10-18 05:02:45 -0400

    Add files via upload

--- world_building_game/map.py
+++ world_building_game/map.py
@@ -8,10 +8,6 @@ from perlin_noise import PerlinNoise
 class Map:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
-        self.map_surface = pygame.Surface((X_SPACER * (COLS + 0.5), Y_SPACER * (ROWS + 0.5)))
-        self.map_rect = pygame.Surface.get_rect(self.map_surface)
-        self.camera_group = pygame.sprite.Group()
-        self.active_tile = None
         self.map_data = self.generate_map()
         self.running = False
         self.grid_check = True
@@ -31,14 +27,13 @@ class Map:
             if (i % 2 != 0):
                 x_pos += X_SCALE
             for j in range(COLS): 
-                new_tile = Tile((i, j), (x_pos, y_pos), self.camera_group)
+                new_tile = Tile((i, j), (x_pos, y_pos))
                 noise_val = noise1([i/ROWS, j/COLS])
                 noise_val += 0.7 * noise2([i/ROWS, j/COLS])
                 noise_val += 0.25 * noise3([i/ROWS, j/COLS])
                 #noise_val += 0.125 * noise4([i/ROWS, j/COLS])
                 #noise_val += 0.0625 * noise5([i/ROWS, j/COLS])
-                new_tile.set_tile_type_color_noise(noise_val)
-                new_tile.set_group(self.camera_group)
+                new_tile.set_tile_type_color(noise_val) 
                 row.append(new_tile)
                 x_pos += X_SPACER
             data.append(row)
@@ -55,58 +50,50 @@ class Map:
         vert_five = current_tile[5]
         vert_six = current_tile[6]
     
-        if(fill == True):
-            pygame.draw.polygon(self.map_surface, color, [vert_one, vert_two, 
+        if fill:
+            pygame.draw.polygon(self.display_surface, color, [vert_one, vert_two, 
                                 vert_three, vert_four, vert_five, vert_six])
         else:
-            pygame.draw.polygon(self.map_surface, color, [vert_one, vert_two, 
+            pygame.draw.polygon(self.display_surface, color, [vert_one, vert_two, 
                                 vert_three, vert_four, vert_five, vert_six], width=2)
 
     def check_mouse(self, pos):
+        check = False
         mouse_xy = pygame.mouse.get_pos()
         sqx = (mouse_xy[0] - pos[0])**2
         sqy = (mouse_xy[1] - pos[1])**2
         if math.sqrt(sqx + sqy) < X_SCALE:
-            return True
-        else:
-            return False
+            check = True
+        return check
 
     def draw_map(self):
-        for i in range(ROWS):
-            for j in range(COLS + 2):
-                if j == 0:
-                    current_tile = self.map_data[i][COLS - 1]
-                elif j == COLS + 1:
-                    current_tile = self.map_data[i][0]
-                else:
-                    current_tile = self.map_data[i][j - 1]
+        for row in self.map_data:
+            for tile in row:
                 fill = True
-                color = current_tile.tile_color
-                self.draw_hexagon(current_tile.vertices, color, fill)
+                color = tile.tile_color
+                self.draw_hexagon(tile.vertices, color, fill)
                 if self.grid_check:
                     color = BLACK
                     fill = False
-                    self.draw_hexagon(current_tile.vertices, color, fill)
+                    self.draw_hexagon(tile.vertices, color, fill)
 
     def run(self):
-        if self.running == False:
+        if not self.running:
             self.draw_map()
-            pygame.image.save_extended(self.map_surface, "map.png")
+            pygame.image.save_extended(self.display_surface, "map.png")
             self.running = True
-        for i in range(ROWS):
-            for j in range(COLS):
-                current_tile = self.map_data[i][j]
-                if self.check_mouse((current_tile.center_x, current_tile.center_y)):
+        for row in self.map_data:
+            for tile in row:
+                if self.check_mouse((tile.center_x, tile.center_y)):
                     color = WHITE
                     fill = False
-                    self.draw_hexagon(current_tile.vertices, color, fill)
+                    self.draw_hexagon(tile.vertices, color, fill)
                     #print(f"{current_tile.noise_value}, {current_tile.tile_type}")
-                    if self.previous_tile != current_tile and self.previous_tile != None:
+                    if self.previous_tile != tile and self.previous_tile != None:
                         fill = False
                         if self.grid_check:
                             color = BLACK
                             self.draw_hexagon(self.previous_tile.vertices, color, fill)
                         else:
                             self.draw_hexagon(self.previous_tile.vertices, self.previous_tile.tile_color, fill)
-                    self.previous_tile = current_tile
-        self.display_surface.blit(self.map_surface, self.map_rect)
+                    self.previous_tile = tile
--- world_building_game/tile.py
+++ world_building_game/tile.py
@@ -3,19 +3,16 @@ import math
 import random
 from settings import *
 
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, index, pos, group):
-        super().__init__(group)
+class Tile():
+    def __init__(self, index, pos):
+        super().__init__()
         self.row_index = index[0]
         self.col_index = index[1]
         self.center_x = pos[0]
         self.center_y = pos[1]
-        self.tile_rect = pygame.Rect((self.center_x - X_SCALE, self.center_y - HEX_SIZE), (X_SCALE * 2, HEX_SIZE * 2))
         self.vertices = self.create_hex()
         self.tile_type = None
         self.tile_color = None
-        self.noise_value = None
-        self.climate = None
 
     def create_hex(self):
         vert_one = (self.center_x + HEX_SIZE * math.cos(math.pi/2), self.center_y + HEX_SIZE * math.sin(math.pi/2))
@@ -27,71 +24,51 @@ class Tile(pygame.sprite.Sprite):
         pos_data = [(self.center_x, self.center_y), vert_one, vert_two, vert_three, vert_four, vert_five, vert_six]
         return pos_data
 
-    def set_tile_type_color_noise(self, noise_val):
-        self.noise_value = noise_val
-        ice = 1
-        h_mountain = 0.55
-        mountain = 0.35
-        hill = 0.15
-        plain = 0.01
-        ocean = -0.05
+    def set_tile_type_color(self, noise_val):
         if self.row_index == 0 or self.row_index == ROWS - 1:
-            self.noise_value = ice
+            noise_val = ICE
         elif self.row_index == 1 or self.row_index == ROWS - 2:
             r_number = random.randint(1, 4)
-            if self.noise_value >= ocean:
-                self.noise_value = ice
+            if noise_val >= OCEAN:
+                noise_val = ICE
             elif r_number != 4:
-                self.noise_value = ice
+                noise_val = ICE
         elif self.row_index == 2 or self.row_index == ROWS - 3:
             r_number = random.randint(1, 3)
             if r_number != 3:
-                self.noise_value = ice
+                noise_val = ICE
             
         elif self.col_index <= 0 or self.col_index >= COLS - 1:
-            if self.noise_value >= plain:
-                self.noise_value -= 0.5
+            if noise_val >= PLAINS:
+                noise_val -= 0.5
         elif self.col_index <= 1 or self.col_index >= COLS - 2:
-            if self.noise_value >= plain:
-                self.noise_value -= 0.25
+            if noise_val >= PLAINS:
+                noise_val -= 0.25
         elif self.col_index <= 3 or self.col_index >= COLS -4:
-            if self.noise_value >= plain:
-                self.noise_value -= 0.15
+            if noise_val >= PLAINS:
+                noise_val -= 0.15
         elif self.col_index <= 5 or self.col_index >= COLS -6:
-            if self.noise_value >= plain:
-                self.noise_value -= 0.1
+            if noise_val >= PLAINS:
+                noise_val -= 0.1
 
-        if self.noise_value == ice:
+        if noise_val == ICE:
             self.tile_type = "ice"
-            self.climate = "ice"
             self.tile_color = WHITE
-        elif self.noise_value >= h_mountain:
+        elif noise_val >= H_MOUNTAIN:
             self.tile_type = "high_mountain"
-            self.climate = "cold"
-            self.tile_color = HIGH_MOUNTAIN
-        elif self.noise_value >= mountain:
+            self.tile_color = HIGH_MOUNTAIN_COLOR
+        elif noise_val >= MOUNTAIN:
             self.tile_type = "mountain"
-            self.tile_color = HILLS
-            self.climate = self.set_climate()
-        elif self.noise_value >= hill:
+            self.tile_color = HILLS_COLOR
+        elif noise_val >= HILLS:
             self.tile_type = "hills"
-            self.tile_color = FOREST
-            self.climate = self.set_climate()
-        elif self.noise_value >= plain:
+            self.tile_color = FOREST_COLOR
+        elif noise_val >= PLAINS:
             self.tile_type = "plains"
-            self.tile_color = PLAINS
-            self.climate = self.set_climate()
-        elif self.noise_value >= ocean:
+            self.tile_color = PLAINS_COLOR
+        elif noise_val >= OCEAN:
             self.tile_type = "ocean"
-            self.climate = "ocean"
-            self.tile_color = WATER
+            self.tile_color = WATER_COLOR
         else:
             self.tile_type = "deep_ocean"
-            self.climate = "ocean"
-            self.tile_color = DEEP_WATER
-    
-    def set_group(self, group):
-        self.group = group
-    
-    def set_climate(self):
-        pass
+            self.tile_color = DEEP_WATER_COLOR
